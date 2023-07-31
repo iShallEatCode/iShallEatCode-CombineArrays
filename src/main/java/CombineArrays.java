b4afa1fb3f8f49f82747436097f3860e61e9d62c
Artem Synytsia <synytsia.artem@yahoo.com>
2023-07-31 15:38:48 +0000

    WkSix_CodingChallenge_Four

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,22 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        // Combined length of the new array
+        int combinedLength = arr1.length + arr2.length;
 
-        return null;
+        // Create new combined array
+        int[] combinedArray = new int[combinedLength];
+
+        // Copy arr1 to combinedArray
+        for (int i = 0; i < arr1.length; i++) {
+            combinedArray[i] = arr1[i];
+        }
+
+        // Copy arr2 to combinedArray
+        for (int i = 0; i < arr2.length; i++) {
+            combinedArray[arr1.length + i] = arr2[i];
+        }
+
+        return combinedArray;
     }
 }
